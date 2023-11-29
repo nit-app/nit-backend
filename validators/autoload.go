fddd5ba114c9a8fc9d03ccc1f4b3c52beb5b57f0
user <haghee0eip6ohjoh@protonmail.com>
2023-11-30 01:30:52 +0500

    validators(phone): add panic if custom validator isn't registered

--- validators/autoload.go
+++ validators/autoload.go
@@ -7,6 +7,11 @@ import (
 
 func init() {
 	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
-		_ = v.RegisterValidation("ruPhoneNumber", ruPhoneNumber)
+		err := v.RegisterValidation("ruPhoneNumber", ruPhoneNumber)
+		if err != nil {
+			panic(err)
+		}
+	} else {
+		panic("cannot register custom validators on engine")
 	}
 }
