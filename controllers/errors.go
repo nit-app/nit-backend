d1d2e4410b7a09c72feab0ef144cadb4ac277709
user <haghee0eip6ohjoh@protonmail.com>
2023-11-30 01:26:20 +0500

    api(errors): add a wrapping layer for inner errors  and the middleware for error handling

---
+++ controllers/errors.go
@@ -0,0 +1,36 @@
+package controllers
+
+import (
+	"errors"
+	"github.com/gin-gonic/gin"
+	wrappedErrors "github.com/nit-app/nit-backend/errors"
+	"github.com/nit-app/nit-backend/models/status"
+	"github.com/nit-app/nit-backend/response"
+	"go.uber.org/zap"
+)
+
+func HandleErrors(c *gin.Context) {
+	c.Next()
+
+	if len(c.Errors) == 0 {
+		return
+	}
+
+	if len(c.Errors) > 1 {
+		zap.L().Error("error handling middleware cannot handle more than one error at once")
+		return
+	}
+
+	var serviceError *wrappedErrors.Error
+	ok := errors.As(c.Errors[0], &serviceError)
+	if !ok {
+		zap.L().Warn("unsolicited non-wrapped error", zap.Error(c.Errors[0]))
+		return
+	}
+
+	if serviceError.Type() == status.InternalServerError {
+		zap.L().Error("internal service error", zap.Error(serviceError.Unwrap()))
+	}
+
+	c.AbortWithStatusJSON(response.ErrorWithText(serviceError.MakeResponse()))
+}
