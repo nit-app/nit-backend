a5463ecea5821ea5d326b13bc52a309856194a50
user <haghee0eip6ohjoh@protonmail.com>
2024-01-11 23:42:05 +0500

    api(cors): bypass built-in middleware

--- cors.go
+++ cors.go
@@ -1,18 +1,9 @@
 package main
 
 import (
-	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
-	"time"
 )
 
 func setCors(r gin.IRoutes) {
-	r.Use(cors.New(cors.Config{
-		AllowMethods:     []string{"GET", "POST", "OPTIONS", "PUT", "PATCH"},
-		AllowHeaders:     []string{"Origin"},
-		ExposeHeaders:    []string{"Content-Length", "Access-Control-Allow-Origin"},
-		AllowAllOrigins:  true,
-		AllowCredentials: true,
-		MaxAge:           12 * time.Hour,
-	}))
+	// bypassed
 }
