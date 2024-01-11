3497733ca3bec70c54007ed0f38f626691038a76
user <haghee0eip6ohjoh@protonmail.com>
2024-01-11 23:30:07 +0500

    api(cors): temporarily adjust cors to return request origin

--- controllers/cors.go
+++ controllers/cors.go
@@ -5,8 +5,20 @@ import (
 	"net/http"
 )
 
+func getAllowedOrigin(c *gin.Context) string {
+	origin := c.GetHeader("Origin")
+	if len(origin) == 0 {
+		origin = c.GetHeader("Referer")
+	}
+	if len(origin) == 0 {
+		origin = "*"
+	}
+
+	return origin
+}
+
 func CORS(c *gin.Context) {
-	c.Header("Access-Control-Allow-Origin", "*")
+	c.Header("Access-Control-Allow-Origin", getAllowedOrigin(c))
 	c.Header("Access-Control-Allow-Credentials", "true")
 	c.Header("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
 	c.Header("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
