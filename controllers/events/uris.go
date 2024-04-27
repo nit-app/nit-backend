2be8e054486a6947477f910ef1a6758269dcd245
user <haghee0eip6ohjoh@protonmail.com>
2024-04-28 01:29:16 +0500

    api(eventAdmin): register handlers for event admin

--- controllers/events/uris.go
+++ controllers/events/uris.go
@@ -2,6 +2,7 @@ package events
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/nit-app/nit-backend/controllers/access"
 	"github.com/nit-app/nit-backend/controllers/util"
 	"github.com/nit-app/nit-backend/models/requests"
 )
@@ -10,4 +11,11 @@ func Register(engine *gin.Engine) {
 	eventsGroup := engine.Group("/v1/events")
 	eventsGroup.POST("/lookup", util.ValidateRequestData[requests.EventLookupFilters], LookupEvents)
 	eventsGroup.GET("/get/:uuid", GetEvent)
+
+	eventAdminGroup := engine.Group("/v1/eventAdmin")
+	eventAdminGroup.Use(access.RequireAdmin)
+
+	eventAdminGroup.GET("/drafts", GetDrafts)
+	eventAdminGroup.POST("/create", CreateDraft)
+	eventAdminGroup.POST("/appendTag", AppendTag)
 }
