dc7e4cecadef9438697100968174301ce0c4b88a
user <haghee0eip6ohjoh@protonmail.com>
2024-04-28 01:17:25 +0500

    schema(event): fix default value for isDraft

--- migrations/20240424181618_add_event_drafts.sql
+++ migrations/20240424181618_add_event_drafts.sql
@@ -1,7 +1,7 @@
 -- +goose Up
 -- +goose StatementBegin
 ALTER TABLE events
-    ADD COLUMN isDraft bool not null default true;
+    ADD COLUMN isDraft bool not null default false;
 ALTER TABLE events
     ADD COLUMN isMachineGenerated bool not null default false;
 -- +goose StatementEnd
