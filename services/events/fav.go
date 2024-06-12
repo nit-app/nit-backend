904c7d8ac94e3674643d6896fb720a767653efcf
user <haghee0eip6ohjoh@protonmail.com>
2024-06-12 22:39:51 +0500

    api(events): implement function to get all favs of a user

---
+++ services/events/fav.go
@@ -0,0 +1,79 @@
+package events
+
+import (
+	"context"
+	"github.com/nit-app/nit-backend/env"
+	"github.com/nit-app/nit-backend/errors"
+	"github.com/nit-app/nit-backend/models"
+	"github.com/nit-app/nit-backend/models/status"
+)
+
+func GetUserFavorites(ctx context.Context, userUUID string) ([]*models.EventHeader, error) {
+	const query = `
+		select
+			e.uuid,
+			e.title,
+			e.priceLow,
+			e.priceHigh,
+			e.ageLimitLow,
+			e.ageLimitHigh,
+			e.location,
+			e.ownerInfo,
+			string_agg(distinct et.tag, ',') as tags,
+			e.createdat,
+			e.modifiedat,
+			coalesce(es.beginsat, '1970-01-01'),
+			coalesce(es.endsat, '1970-01-01'),
+			coalesce(es.addedat, '1970-01-01'),
+			coalesce(es.scheduleuuid, '00000000-0000-0000-0000-000000000000'),
+			e.hascertificate,
+			e.plainDescription,
+			e.favcount,
+			e.isDraft,
+			e.isMachineGenerated
+		from
+			events e
+		inner join event_favorites on 
+		    e.uuid = event_favorites.eventuuid
+		left join event_tags et on
+			e.uuid = et.uuid
+		left join event_schedule es on
+			e.uuid = es."eventUuid"
+		where
+		    not e.isDraft
+		  	and event_favorites.useruuid = $1
+			and e.deletedat is null
+		group by
+			e.uuid,
+			e.favcount,
+			es.scheduleuuid,
+			es.addedat,
+			event_favorites.addedat,
+			es.beginsat,
+			es.endsat
+		order by event_favorites.addedat desc`
+
+	rows, err := env.DB().QueryContext(ctx, query, userUUID)
+	if err != nil {
+		return nil, errors.New(status.InternalServerError, err)
+	}
+
+	defer rows.Close()
+
+	eventHeaders := make([]*models.EventHeader, 0)
+	for rows.Next() {
+		eventObject, err := ScanEventHeader(rows, nil)
+
+		if eventObject.IsDraft {
+			continue
+		}
+
+		if err != nil {
+			return nil, errors.New(status.InternalServerError, err)
+		}
+
+		eventHeaders = append(eventHeaders, eventObject)
+	}
+
+	return eventHeaders, nil
+}
